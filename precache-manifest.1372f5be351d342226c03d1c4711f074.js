2ac9a46c685a664c1636da748fd8dd904eb35053
Mahedi Kamal <mahedikamal44@gmail.com>
2020-11-21 16:09:40 +0600

    Updates

---
+++ precache-manifest.1372f5be351d342226c03d1c4711f074.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "822c29ade4d0e7c5f918a5146fa6dc3c",
+    "url": "/GREFreeSupport/index.html"
+  },
+  {
+    "revision": "571dd4ecf8d3c40b57eb",
+    "url": "/GREFreeSupport/static/css/main.25831fbe.chunk.css"
+  },
+  {
+    "revision": "a16ab51b2818e762edc0",
+    "url": "/GREFreeSupport/static/js/2.e556e86d.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/GREFreeSupport/static/js/2.e556e86d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "571dd4ecf8d3c40b57eb",
+    "url": "/GREFreeSupport/static/js/main.3e32cc47.chunk.js"
+  },
+  {
+    "revision": "45051d5bd5883e152e18",
+    "url": "/GREFreeSupport/static/js/runtime-main.63285326.js"
+  }
+]);
