7ae4907b84002df0ef05a58633ad5a5a9a7a7dbf
Mahedi Kamal <mahedikamal44@gmail.com>
2020-11-22 14:05:09 +0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/GREFreeSupport/precache-manifest.25a9eec1f062fa0696daaea9552e8168.js"
+  "/GREFreeSupport/precache-manifest.5c60676eadc3e7401dff8d80f02cf0b9.js"
 );
 
 self.addEventListener('message', (event) => {
