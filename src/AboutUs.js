28c7f3617f8cb959c61a27e3ada5aecf96252a23
Mahedi Kamal <mahedikamal44@gmail.com>
2020-11-22 09:08:22 +0600

    changed about us page

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -5,6 +5,18 @@ import Typical from 'react-typical'
 import Aos from 'aos';
 import "aos/dist/aos.css";
 import img from './MyImage/pageimg.PNG';
+import img1 from './MyImage/p1.PNG';
+import img2 from './MyImage/p2.PNG';
+import img3 from './MyImage/p3.PNG';
+import img4 from './MyImage/p4.PNG';
+import img5 from './MyImage/p5.PNG';
+import { AiOutlineFacebook } from "react-icons/ai";
+import { AiFillMail } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
+import { AiOutlineDribbble } from "react-icons/ai";
+import { AiOutlineGithub } from "react-icons/ai";
+
+import Carousel from 'react-elastic-carousel';
 
 
 
@@ -21,10 +33,35 @@ import img from './MyImage/pageimg.PNG';
         
         return(
             <div className="front-page-background">
-                <img src={img} alt="question"/>
+                <h2>
+                    <Typical
+                        steps={[' ', 1000, 'Developed by : Mahedi Kamal', 500]}
+                        loop={Infinity}
+                        wrapper="p"
+                    />
+                </h2>
+                <Carousel>
+                    <div className="img-chard">
+                    <img src={img1} alt="question"/>
+                    </div>
+                    <div className="img-chard">
+                    <img src={img2} alt="question"/>
+                    </div>
+                    <div className="img-chard">
+                    <img src={img3} alt="question"/>
+                    </div>
+                    <div className="img-chard">
+                    <img src={img4} alt="question"/>
+                    </div>
+                    <div className="img-chard">
+                    <img src={img5} alt="question"/>
+                    </div>
+                </Carousel>
+
+
                 <div className="animation-text">
                     <Typical
-                        steps={['W ', 1000, 'We are honoured to help you !', 500]}
+                        steps={['W ', 1000, 'We are glad to help you !', 500]}
                         loop={Infinity}
                         wrapper="p"
                     />
@@ -32,7 +69,9 @@ import img from './MyImage/pageimg.PNG';
                 <br/><br/><br/>
                 <div className="grids">
                     <div data-aos="fade-up" className="about-us-box">
-                        about us bos
+                        This software is not affiliated anyway with ETS.This doesn't 
+                        belong to any organisation. This is a self-initiated project
+                        by Mahedi Kamal(Avash)
                     </div>
                     <br/><br/>
                     <div data-aos="fade-right" className="about-us-box">
@@ -40,7 +79,25 @@ import img from './MyImage/pageimg.PNG';
                     </div>
                     <br/><br/>
                     <div data-aos="fade-up" className="about-us-box">
-                        about us bos
+                        Contact us:<br/>
+                        <Link to="/GREPreparationCenter/gREPreparationCenter">
+                            <i> <AiOutlineFacebook size={25}/> </i>
+                        </Link><br/>
+                        <Link to="/GREPreparationCenter/gREPreparationCenter">
+                            <i> <AiFillMail size={25}/> </i>
+                        </Link>
+                        <br/>
+                        <Link to="/GREPreparationCenter/gREPreparationCenter">
+                            <i> <AiFillLinkedin size={25}/> </i>
+                        </Link>
+                        <br/>
+                        <Link to="/GREPreparationCenter/gREPreparationCenter">
+                            <i> <AiOutlineDribbble size={25}/> </i>
+                        </Link>
+                        <br/>
+                        <Link to="/GREPreparationCenter/gREPreparationCenter">
+                            <i> <AiFillLinkedin size={25}/> </i>
+                        </Link>
                     </div><br/>
                 </div>
             </div>
